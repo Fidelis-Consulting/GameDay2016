f54254ecc35c94c4d957606707a4e8968ed05555
Mark Saum <mark@modopayments.com>
2016-11-28 17:00:26 -0800

    updated sqs python client

--- sqs.py
+++ sqs.py
@@ -1,16 +1,35 @@
 #!/usr/bin/env python
 
+import json
+
 import boto3
 
-# Choosing the resource from boto3 module
-sqs = boto3.resource('sqs')
+# import redis
 
-# Get the queue named test
-queue = sqs.get_queue_by_name(QueueName='disposableunicorns')
-
-# Process messages by printing out body from test Amazon SQS Queue
-for message in queue.receive_messages():
-    print('Hello, {0}'.format(message.body))
-    #message.delete()
+# logging.basicConfig(level=logging.DEBUG)
 
 
+
+if __name__ == "__main__":
+
+    # Choosing the resource from boto3 module
+    sqs = boto3.resource('sqs')
+    ec = boto3.client('elasticache')
+
+    # Get the queue named test
+    queue = sqs.get_queue_by_name(QueueName='disposableunicorns')
+
+    # Process messages by printing out body from test Amazon SQS Queue
+    # while True:
+    for msg in queue.receive_messages():
+        print (format(msg.body))
+
+    data = json.loads(msg.body)
+
+    print ('TotalParts: ' + str(data['TotalParts']))
+    print ('PartNumber: ' + str(data['PartNumber']))
+    print ('Id: ' + data['Id'])
+    print ('Data: ' + data['Data'])
+
+
+    # message.delete()
