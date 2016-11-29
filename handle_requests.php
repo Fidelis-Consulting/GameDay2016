618614cf8e2f94e5651859d2b8afa6ebd984a3e2
Jason Westbrook <jwestbrook@gmail.com>
2016-11-28 17:16:51 -0800

    used wrong std in url

--- handle_requests.php
+++ handle_requests.php
@@ -13,9 +13,8 @@
 $redis = new Redis();
 $redis->connect('unicorns.a0yryy.0001.euc1.cache.amazonaws.com', 6379);
 
-$data = file_get_contents('php://stdin');
+$data = file_get_contents('php://input');
 
 $data = json_decode($data);
 
 $redis->lPush($data->Id,$data);
-
