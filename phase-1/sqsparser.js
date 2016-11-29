3bf0f049d1361b3b1d17b1ba34bd40de1a696595
joe42- <jpemberton@consultantjoes.com>
2016-11-28 20:24:52 -0700

    url

--- phase-1/sqsparser.js
+++ phase-1/sqsparser.js
@@ -2,19 +2,19 @@ var AWS = require('aws-sdk');
 
 AWS.config.update({
     region: 'eu-central-1',
-    accessKeyId: 'ASIAJ66UXXPGA4GV2N5Q',
-    secretAccessKey: 'mCogc+xwjDmgbKFXWJcblOhQ+BYt3rsaFxqIYd4o',
-    sessionToken: 'FQoDYXdzEDwaDBNp3sAf5zBj9RSVTyLZAZUYgjjgdovV7vD237bHpXZkZRcB+meiNTT0tylOnS9LZX95yM1uKnQQAUXAkpkOp4BH0DXady5V5flxGv0TgdFP2HgT7+yZoQQi3WVS/Fto/b+HzGguYOH0r9imY4YNKe1EZiPR1soRjbIFtMZ3gQoffwaPyT+DEkha2pdY6hOPuvPxcoTEOsmU5+F6B7k+kHWxDeSGZ6tw+DzS7VCHfbbUvKYMiCPH/Y15JYbdH+nr19lokF704Nn2D22SyzsI/Mdpn9CsqArnk5yWvxQjZNmc/fAvKS4hqqgo8t7zwQU='
+    accessKeyId: 'ASIAI5VNHXN75IDGQFMQ',
+    secretAccessKey: 'AaECNm8iVBb4v0yIFCsnMNeH07+FCmXyHZDrrodm',
+    sessionToken: 'FQoDYXdzEDwaDIuSD0ddrJTNEWxO4SLZAZ7jC1csAm2cBvfQhIlEEeVMLqh5CVX0lxzEvhALYkvLSzoj44SpCW4Ldy9/uKz0n6Qi+WhhU+qF28knAKACRbil1Pn+LzhXKDp+MsQrSZdq0twaKWU1IPQkqOAwfgKZv6EO+pLiHXOIzsFteUJMYQr2pp4ibuHS1ZB+NhsC8joWxGsIlldfRff5KCKX5+nN99F8/gFI438IoF156yJRwNt29g0FvZYLY9m0Ex99wZDBL9q454vwwTO83pJlfb6bPmAi+t2kpnRv6gpJzcoTsEISIp/R5sQMlRsoneTzwQU='
 });
 
 
 var Consumer = require('sqs-consumer');
 var redis = require('redis');
-var host = "unicorns.a0yryy.0001.euc1.cache.amazonaws.com";
+var host = "unicorns3.tvmx6i.0001.euc1.cache.amazonaws.com";
 var port = 6379;
 
 var app = Consumer.create({
-    queueUrl: 'https://sqs.eu-central-1.amazonaws.com/896481443273/disposableunicorns',
+    queueUrl: 'https://sqs.eu-central-1.amazonaws.com/748244679595/disposableunicorns2',
     region: 'eu-central-1',
     batchSize: 10,
     handleMessage: function (message, done) {
